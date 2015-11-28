b6f477fc36e804367c098931c20d7c894dec1f58
amekusa <post@amekusa.com>
2015-11-28 16:10:51 +0900

    Update 2015-11-28T07:10:47.063Z

--- elementlist.js
+++ elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","amekusa\\plz\\alt"],["c","amekusa\\plz\\arr"],["c","amekusa\\plz\\constant"],["c","amekusa\\plz\\fn"],["c","amekusa\\plz\\LocalException"],["c","amekusa\\plz\\main"],["c","amekusa\\plz\\num"],["c","amekusa\\plz\\obj"],["c","amekusa\\plz\\op"],["c","amekusa\\plz\\path"],["c","amekusa\\plz\\RecoverableError"],["c","amekusa\\plz\\str"],["c","amekusa\\plz\\sys"],["c","amekusa\\plz\\T"],["c","amekusa\\plz\\type"],["c","ErrorException"],["c","Exception"]];
+ApiGen.elements = [["c","amekusa\\plz\\alt"],["c","amekusa\\plz\\arr"],["c","amekusa\\plz\\constant"],["c","amekusa\\plz\\fn"],["c","amekusa\\plz\\LocalException"],["c","amekusa\\plz\\num"],["c","amekusa\\plz\\obj"],["c","amekusa\\plz\\path"],["c","amekusa\\plz\\RecoverableError"],["c","amekusa\\plz\\str"],["c","amekusa\\plz\\sys"],["c","amekusa\\plz\\T"],["c","amekusa\\plz\\type"],["c","ErrorException"],["c","Exception"]];
