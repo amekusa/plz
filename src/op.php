ec9677fddfaf72b360ac9a07bae1a3b644e6ad94
amekusa <post@amekusa.com>
2015-12-24 14:21:42 +0900

    Add methods: op::any(), op::all()

--- src/op.php
+++ src/op.php
@@ -1,12 +1,17 @@
 <?php namespace amekusa\plz;
 
 /**
- * @ignore
+ * Operator utilities
+ *
+ * To get started, place the following line around top of your code.
+ * ```php
+ * use amekusa\plz\op;
+ * ```
  */
 abstract class op {
 
 	/**
-	 * Returns whether or not X equals Y
+	 * Returns whether or not `$X` equals `$Y`
 	 * @param mixed $X
 	 * @param mixed $Y
 	 * @return boolean
@@ -36,5 +41,109 @@ abstract class op {
 			}
 		}
 		return $X === $Y;
+	}
+
+	/**
+	 * Returns whether any one of conditions supplied is *truthy*
+	 *
+	 * If only 1 argument is passed and it is *iterable*,
+	 * checks whether any one of its elements is *truthy*
+	 *
+	 * @example Basic usage
+	 * ```php
+	 * $var1 = null;     // Falsy
+	 * $var2 = 0;        // Falsy
+	 * $var3 = 'string'; // Truthy
+	 * if (op::any($var1, $var2, $var3)) {
+	 *   echo 'true';
+	 * } else {
+	 *   echo 'false';
+	 * }
+	 * ```
+	 * ```html
+	 * true
+	 * ```
+	 * @example Checking iterable elements
+	 * ```php
+	 * $var = array (
+	 *   null,    // Falsy
+	 *   0,       // Falsy
+	 *   'string' // Truthy
+	 * );
+	 * if (op::any($var)) {
+	 *   echo 'true';
+	 * } else {
+	 *   echo 'false';
+	 * }
+	 * ```
+	 * ```html
+	 * true
+	 * ```
+	 * @param mixed[*] $Conditions
+	 * @return boolean
+	 */
+	static function any($Conditions) {
+		if (func_num_args() == 1 && type::is_iterable($Conditions)) {
+			foreach ($Conditions as $iCond) {
+				if (type::bool($iCond)) return true;
+			}
+			return false;
+		}
+		foreach (func_get_args() as $iArg) {
+			if (type::bool($iArg)) return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Returns whether all of conditions supplied is *truthy*
+	 *
+	 * If only 1 argument is passed and it is *iterable*,
+	 * checks whether all of its elements is *truthy*
+	 *
+	 * @example Basic usage
+	 * ```php
+	 * $var1 = null;     // Falsy
+	 * $var2 = 0;        // Falsy
+	 * $var3 = 'string'; // Truthy
+	 * if (op::all($var1, $var2, $var3)) {
+	 *   echo 'true';
+	 * } else {
+	 *   echo 'false';
+	 * }
+	 * ```
+	 * ```html
+	 * false
+	 * ```
+	 * @example Checking iterable elements
+	 * ```php
+	 * $var = array (
+	 *   null,    // Falsy
+	 *   0,       // Falsy
+	 *   'string' // Truthy
+	 * );
+	 * if (op::all($var)) {
+	 *   echo 'true';
+	 * } else {
+	 *   echo 'false';
+	 * }
+	 * ```
+	 * ```html
+	 * false
+	 * ```
+	 * @param mixed[*] $Conditions
+	 * @return boolean
+	 */
+	static function all($Conditions) {
+		if (func_num_args() == 1 && type::is_iterable($Conditions)) {
+			foreach ($Conditions as $iCond) {
+				if (!type::bool($iCond)) return false;
+			}
+			return true;
+		}
+		foreach (func_get_args() as $iArg) {
+			if (!type::bool($iArg)) return false;
+		}
+		return true;
 	}
 }
